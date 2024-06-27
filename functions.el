836d464e18e18b10699f13c85e368749444bf2b0
Luyang Liu <luyang.l@proton.me>
2024-06-27 15:11:12 +1000

    Added functions files.

---
+++ functions.el
@@ -0,0 +1,101 @@
+(defvar-local hidden-mode-line-mode nil
+  "Non-nil if Hidden-Mode-Line mode is enabled.")
+
+(defvar hidden-mode-line-format nil
+  "Store the current mode-line-format.")
+
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hidden-mode-line-format mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hidden-mode-line-format
+          hidden-mode-line-format nil))
+  (force-mode-line-update)
+  (redraw-display))
+
+(defun luyangliuable/toggle-mode-line ()
+  "Toggle the modeline on and off."
+  (interactive)
+  (if hidden-mode-line-mode
+      (hidden-mode-line-mode -1)
+    (hidden-mode-line-mode 1)))
+
+(defun luyangliuable/toggle-absolute-line-numbers ()
+  "Toggle between absolute line numbers and no line numbers."
+  (interactive)
+  (if (eq display-line-numbers t)
+      (setq display-line-numbers nil)
+    (setq display-line-numbers t))
+  (redraw-display))
+
+(defun luyangliuable/toggle-relative-line-numbers ()
+  "Toggle between relative line numbers and no line numbers."
+  (interactive)
+  (if (eq display-line-numbers 'relative)
+      (setq display-line-numbers nil)
+    (setq display-line-numbers 'relative))
+  (redraw-display))
+
+(defun luyangliuable/goto-scratch-buffer ()
+  "Switch to the *scratch* buffer."
+  (interactive)
+  (switch-to-buffer "*scratch*"))
+
+(defun luyangliuable/split-window-right-and-run-callback (callback)
+  "Split the window vertically and run the CALLBACK function in the new window."
+  (interactive "aFunction to run in new window: ")
+  (split-window-right)
+  (other-window 1)
+  (funcall callback))
+
+(defhydra hydra-window-management (:color amaranth :hint nil)
+  "
+Movement^^        ^Split^         ^Delete^        ^Other^
+----------------------------------------------------------------
+_h_: left         _v_: vertical   _d_: delete     _u_: undo
+_j_: down         _s_: horizontal _o_: other      _r_: redo
+_k_: up           _m_: maximize   _D_: delete     _b_: balance
+_l_: right        _}_: minimize
+_f_: follow       _{_: enlarge    _F_: fullscreen
+_o_: other        _w_: ace-window
+"
+  ("h" windmove-left)               ; Move focus to the left window
+  ("j" windmove-down)               ; Move focus to the window below
+  ("k" windmove-up)                 ; Move focus to the window above
+  ("l" windmove-right)              ; Move focus to the right window
+  ("v" split-window-right)          ; Split the window vertically
+  ("s" split-window-below)          ; Split the window horizontally
+  ("d" delete-window)               ; Delete the current window
+  ("D" delete-other-windows)        ; Delete all other windows
+  ("m" delete-other-windows)        ; Maximize the current window
+  ("M" minimize-window)             ; Minimize the current window
+  ("]" enlarge-window-horizontally) ; Enlarge the window horizontally
+  ("[" shrink-window-horizontally)  ; Shrink the window horizontally
+  ("{" shrink-window-horizontally)  ; Shrink the window horizontally
+  ("}" enlarge-window-horizontally) ; Enlarge the window horizontally
+  ("f" follow-mode)                 ; Toggle follow mode
+  ("o" other-window)                ; Switch to the other window
+  ("b" balance-windows)             ; Balance the sizes of all windows
+  ("F" toggle-frame-fullscreen)     ; Toggle fullscreen mode
+  ("u" winner-undo)                 ; Undo window configuration change
+  ("r" winner-redo)                 ; Redo window configuration change
+  ("w" ace-window)                  ; Select window with ace-window
+  ("q" nil "quit" :color blue))     ; Quit the hydra
+
+(defun luyangliuable/switch-to-last-buffer ()
+  "Switch back and forth between current and last buffer in the current window."
+  (interactive)
+  (let ((current-buffer (current-buffer))
+        (last-buffer (other-buffer (current-buffer) 1)))
+    (switch-to-buffer last-buffer)))
+
+(defun luyangliuable/split-window-below-and-run-callback (callback)
+  "Split the window vertically and run the CALLBACK function in the new window."
+  (interactive "Function to run in new window: ")
+  (split-window-below)
+  (other-window 1)
+  (funcall callback))
